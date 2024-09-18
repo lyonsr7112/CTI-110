9fb1f8a9ffd56622f83b8854e172b3d82b115ede
lyonsr7112 <lyonsr7112@student.faytechcc.edu>
2024-09-18 11:02:27 -0400

    Add files via upload

--- P1HW1_LyonsRashun.py
+++ P1HW1_LyonsRashun.py
@@ -4,6 +4,8 @@
 # Using math expressions with user input
 
 print("--------Calculating Exponents--------")
+print()
+print()
 
 base_value = input("Enter an integer as the base value: ")
 
@@ -12,7 +14,26 @@ base_value = int(base_value)
 
 # Get input from user and convert to integer
 exponent = int(input("Enter an integer as the exponent: "))
+print()
+print()
 
 # Display math result to the user
-print(base_value, "raised to the power of", exponent, "is", base_value**exponent)
+print(base_value, "raised to the power of", exponent, "is", str(base_value**exponent) + "!!")
+print()
+print()
 
+print("----------Addition and Subtraction----------")
+print()
+print()
+
+# Get integers from users
+start_num = int(input("Enter a starting integer: "))
+add_num = int(input("Enter an integer to add: "))
+sub_num = int(input("Enter an integer to subtract: "))
+print()
+print()
+# Calculate the result
+result = start_num + add_num - sub_num
+
+# Display all variables and result
+print(start_num, "+", add_num, "-", sub_num, "is equal to", result)
